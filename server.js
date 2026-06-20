bfb5e35f7c16c01718de0a6e0828e47e426d2e03
Christin Babu <christinbabu42@gmail.com>
2026-06-21 05:19:12 +0530

    Update CORS for production

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const PORT = process.env.PORT || 5000;
 // -----------------------------------------------------------------------------
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
