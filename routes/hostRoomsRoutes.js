9a5fbe8c9230e0605a4e372e80cdbf7aa193d5a5
Christin Babu <christinbabu42@gmail.com>
2026-07-12 08:32:52 +0530

    add

--- routes/hostRoomsRoutes.js
+++ routes/hostRoomsRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const BookingRoom = require("../models/BookingRoom");
 const DogStay = require("../models/DogStay");
-const auth = require("../middleware/auth");
+const auth = require("../middlewares/auth");
 
 const router = express.Router();
 
